0ce72e2f55a81731cdf0d1ec9914436c57e8b67d
olhonolance10oficial <olhonolance10oficial@gmail.com>
2026-08-11 10:08:38 -0300

    Update site-logo.tsx

--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -10,7 +10,7 @@ export function SiteLogo({ className = "" }: { className?: string }) {
       <img
         src="/logo_olho_no_lance.png"
         alt="Olho no Lance"
-        className="h-16 w-auto object-contain"
+        className="h-28 w-auto object-contain"
       />
     </Link>
   )
